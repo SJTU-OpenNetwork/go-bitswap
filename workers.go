2d780cac211ce4a1db05fe8a5b2243490ee55363
Riften <943275366@qq.com>
2019-12-18 21:47:46 +0800

    add BLKSEND

--- workers.go
+++ workers.go
@@ -155,7 +155,8 @@ func (bs *Bitswap) sendBlocks(ctx context.Context, env *engine.Envelope) {
 	for _, block := range env.Message.Blocks() {
 		msgSize += len(block.RawData())
 		msg.AddBlock(block)
-		log.Infof("Sending block %s to %s", block, env.Peer)
+		//log.Infof("Sending block %s to %s", block, env.Peer)
+		log.Debugf("[BLKSEND] Cid %s, SendTo %s", block, env.Peer)
 	}
 
 	bs.sentHistogram.Observe(float64(msgSize))
